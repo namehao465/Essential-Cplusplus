6500e1807de35ab4af6ee942d1f79faffe384557
namehao465 <36102519+namehao465@users.noreply.github.com>
2019-09-21 09:07:47 +0800

    init20190921

---
+++ 04 Object-Based Programming/04 Object-Based Programming/Stack.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<vector>
+using namespace std;
+class Stack {
+public:
+	bool push(const string&);
+	bool pop(string &elem);
+	bool peek(string &elem);
+
+	bool empty();
+	bool full();
+
+	int size() { return _stack.size(); }
+private:
+	vector<string> _stack;
+};
